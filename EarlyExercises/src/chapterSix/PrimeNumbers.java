297dd4b8b05395ebc182467b5e18b0393a2ba467
Ademiju Taiwo <hardaymyju@gmail.com>
2022-01-12 01:42:45 +0100

    updated files

--- EarlyExercises/src/chapterSix/PrimeNumbers.java
+++ EarlyExercises/src/chapterSix/PrimeNumbers.java
@@ -7,38 +7,54 @@ public class PrimeNumbers {
         System.out.println("Enter a number to check for: ");
         Scanner inputCollector = new Scanner(System.in);
         int userInput = inputCollector.nextInt();
+        determinePrimeNumbers(userInput);
+        System.out.println();
 
-        displayPrimeNumbers(userInput);    }
-    public static void displayPrimeNumbers(int number){
-        int factor;
-        int factorCount = 0;
-        int divisor = 0;
-        if(number % 2 != 0) {
-            while (divisor <= number) {
-                divisor++;
-                if (number % divisor == 0) {
-                    factor = divisor;
-
-                    if (factor == 1) {
-                        factorCount++;
-                    }
-                    if (factor == number) {
-                        factorCount++;
-                    }
-                }
-
-            }
-
-
-        }else{
-                System.out.printf("%d is not a prime number ", number);
-            }
-
-            if (factorCount ==2){
-            System.out.printf("%d is a Prime number ",number);
-        }else {
-                System.out.printf("%d is not a prime number ", number);
-
-            }
+        System.out.println("Prime numbers between 1 and 1000 :");
+        for (int number = 1; number <= 1000; number++) {
+            displayPrimeNumbers(number);
+        }
     }
-}
+
+
+    public static void determinePrimeNumbers(int number) {
+        int nonPrimeNumberCounter = 0;
+        for (int divisor = 2; divisor <= (Math.sqrt(number)); divisor++) {
+            if (number % divisor == 0) {
+                nonPrimeNumberCounter++;
+            }
+        }
+
+        if (number == 1) {
+            System.out.printf("%d is not a Prime number", number);
+        } else if (nonPrimeNumberCounter == 0) {
+            System.out.printf("%d is a Prime number ", number);
+
+        } else {
+            System.out.printf("%d is not a prime number ", number);
+        }
+    }
+
+    public static void displayPrimeNumbers(int number) {
+        int factorCounter = 0;
+        for (int divisor = 2; divisor <= (Math.sqrt(number)); divisor++) {
+
+            if (number % divisor == 0) {
+                factorCounter++;
+            }
+        }
+            if(number == 1){
+                System.out.printf("Note that %d is not a Prime number%n", number);
+
+            }
+            else if (factorCounter == 0) {
+                System.out.printf("%4d ", number);
+
+            }
+
+        }
+    }
+
+
+
+
